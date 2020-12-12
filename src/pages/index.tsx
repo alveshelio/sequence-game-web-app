62f5bf21ca3c516f82788eb9417c587e94c538c6
Helio <helios25@gmail.com>
2020-12-12 23:07:30 +0000

    :truck: added deck card images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import Layout from '@components/layout/Layout'
-import Input from '@uiKit/input/Input'
+import Board from '@components/game/Board'
 
 export default function Home(): React.ReactElement {
   return (
     <Layout title="Sequence Game">
-      <h2 className="text-3xl">What&apos;s new</h2>
-      <Input hasHover={true} />
+      <Board />
     </Layout>
   )
 }
