fbe6e9abab634cac3f9ba6411977b52586aa378a
Helio <helios25@gmail.com>
2020-12-13 08:01:37 +0000

    :lipstick: created Chip component

---
+++ src/uiKit/chip/Chip.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { styled } from 'twin.macro'
+
+interface ChipProps {
+  color: string
+}
+
+const StyledChip = styled.div<ChipProps>`
+  position: absolute;
+  top: calc(50% - 20px);
+  left: calc(50% - 20px);
+  width: 40px;
+  height: 40px;
+  background-color: ${({ color }) => color};
+  border-radius: 100%;
+  box-shadow: 2px 5px 0 rgba(0, 0, 0, 0.4);
+  z-index: 20;
+
+  &:after {
+    content: '';
+    width: 25px;
+    height: 25px;
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.1);
+    top: 50%;
+    left: 50%;
+    transform: translatex(-50%) translatey(-50%);
+    border-radius: 100%;
+    box-shadow: 0 5px 0 rgba(0, 0, 0, 0.2);
+  }
+`
+
+const Chip: React.FC<ChipProps> = ({ color }: ChipProps): React.ReactElement => {
+  return <StyledChip color={color} />
+}
+
+export default Chip
