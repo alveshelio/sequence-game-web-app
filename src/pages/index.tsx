c256dd479a54e9675dc92c50333042219d645dcf
Helio <helios25@gmail.com>
2020-12-15 10:14:53 +0000

    :sparkles: using new component to create a game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
+import NewGame from '@components/newGame/NewGame'
 import React from 'react'
 
 import Layout from '@components/layout/Layout'
-import Board from '@components/board/Board'
 
 export default function Home(): React.ReactElement {
   return (
     <Layout title="Sequence Game">
-      <Board />
+      <NewGame />
     </Layout>
   )
 }
