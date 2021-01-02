c486298801be7bfbc09a4da1c00467525e10a33e
Helio <helios25@gmail.com>
2021-01-02 13:29:41 +0000

    :sparkles: Added Apollo Provider with initial cache and initial Nhost config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,18 @@ import { NhostApolloProvider } from '@nhost/react-apollo'
 
 import { auth } from 'libs/nhost'
 import { GlobalStyles } from 'twin.macro'
+import { cache } from 'cache/cache'
 
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }): React.ReactElement {
   return (
     <NhostAuthProvider auth={auth}>
-      <NhostApolloProvider auth={auth} gqlEndpoint={process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}>
+      <NhostApolloProvider
+        cache={cache}
+        auth={auth}
+        gqlEndpoint={process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}
+      >
         <GlobalStyles />
         <Component {...pageProps} />
       </NhostApolloProvider>
