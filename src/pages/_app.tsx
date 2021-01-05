b625f16f4ed60b696e1f3116642809b2c41ca9cb
Helio <helios25@gmail.com>
2021-01-05 17:05:13 +0000

    :sparkles: Page to create new game

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { NhostApolloProvider } from '@nhost/react-apollo'
 import { auth } from 'libs/nhost'
 import { GlobalStyles } from 'twin.macro'
 import { cache } from 'cache/cache'
-
 import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }): React.ReactElement {
---
+++ src/pages/game/new.tsx
@@ -0,0 +1,10 @@
+import Layout from '@components/layout/Layout'
+import Teams from '@components/team/Teams'
+
+export default function CreateNewGame() {
+  return (
+    <Layout title="New Game">
+      <Teams />
+    </Layout>
+  )
+}
