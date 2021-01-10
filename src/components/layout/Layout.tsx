bc24ebb4c76ca8c93c711998e63bf9ab95fd2812
Helio <helios25@gmail.com>
2021-01-10 14:22:53 +0000

    :poop: added menu to layout but needs to be moved into its own component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,25 @@
 import Head from 'next/head'
 import React from 'react'
+import { Button } from 'reakit/Button'
 import Link from 'next/link'
+import { useAuth } from '@nhost/react-auth'
+import { auth } from 'libs/nhost'
+import { useRouter } from 'next/router'
+
 interface LayoutProps {
   children: React.ReactNode
   title: string
 }
 const Layout: React.FC<LayoutProps> = ({ children, title }: LayoutProps): React.ReactElement => {
+  const router = useRouter()
+  const { signedIn } = useAuth()
+
+  console.warn('signedIn', signedIn)
+
+  const handleLogout = async (): Promise<void> => {
+    await auth.logout()
+    router.push('/')
+  }
   return (
     <div className="grid grid-rows-layout mx-4">
       <Head>
@@ -29,13 +43,24 @@ const Layout: React.FC<LayoutProps> = ({ children, title }: LayoutProps): React.
                 </a>
               </Link>
             </li>
-            <li>
-              <Link href="/login">
-                <a className="border border-blue-200 hover:border-blue-600 bg-blue-500 text-white hover:text-blue-100 py-2 px-2 rounded-md">
-                  Login
-                </a>
-              </Link>
-            </li>
+            {signedIn === null ? null : !signedIn ? (
+              <li>
+                <Link href="/login">
+                  <a className="border border-blue-200 hover:border-blue-600 bg-blue-500 text-white hover:text-blue-100 py-2 px-2 rounded-md">
+                    Login
+                  </a>
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Button
+                  onClick={handleLogout}
+                  className="border border-blue-200 hover:border-blue-600 bg-blue-500 text-white hover:text-blue-100 py-2 px-2 rounded-md"
+                >
+                  Logout
+                </Button>
+              </li>
+            )}
           </ul>
         </nav>
       </header>
